16041a261a97c40977fcbe7c7f0b2e08ecbbc84a
tepavcevic <djordje.tepa@gmail.com>
2023-08-26 16:16:10 +0200

    Day two (after a couple of days )

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,3 +1,83 @@
-export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
+const dir = [
+    [-1, 0],
+    [1, 0],
+    [0, -1],
+    [0, 1],
+];
 
+function walk(
+    maze: string[],
+    wall: string,
+    current: Point,
+    end: Point,
+    seen: boolean[][],
+    path: Point[],
+): boolean {
+    //1. base case
+    //check if we are off the map
+    if (
+        current.x < 0 ||
+        current.x > maze[0].length ||
+        current.y < 0 ||
+        current.y > maze.length
+    )
+        return false;
+
+    //check if we walked into a wall
+    if (maze[current.y][current.x] === wall) return false;
+
+    //check if we are on the end
+    if (current.x === end.x && current.y === end.y) {
+        path.push(end);
+        return true;
+    }
+
+    //check if we've seen this point
+    if (seen[current.y][current.x]) return false;
+
+    //pre
+    seen[current.y][current.x] = true;
+    path.push(current);
+
+    //recurse
+    for (let i = 0; i < dir.length; i++) {
+        const [x, y] = dir[i];
+        if (
+            walk(
+                maze,
+                wall,
+                {
+                    x: current.x + x,
+                    y: current.y + y,
+                },
+                end,
+                seen,
+                path,
+            )
+        )
+            return true;
+    }
+
+    //post
+    path.pop();
+
+    return false;
+}
+
+export default function solve(
+    maze: string[],
+    wall: string,
+    start: Point,
+    end: Point,
+): Point[] {
+    const seen: boolean[][] = [];
+    const path: Point[] = [];
+
+    for (let i = 0; i < maze.length; i++) {
+        seen.push(new Array(maze[0].length).fill(false));
+    }
+
+    walk(maze, wall, start, end, seen, path);
+
+    return path;
 }
--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,48 @@
+type Node<T> = {
+    value: T;
+    prev?: Node<T>;
+};
+
 export default class Stack<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
 
     constructor() {
+        this.head = undefined;
+        this.length = 0;
     }
 
     push(item: T): void {
+        this.length++;
+        const node = { value: item } as Node<T>;
 
-}
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
+
+        node.prev = this.head;
+        this.head = node;
+    }
     pop(): T | undefined {
+        if (!this.head) return undefined;
 
-}
+        this.length--;
+
+        const head = this.head;
+
+        if (this.length === 0) {
+            this.head = undefined;
+            return head.value;
+        }
+
+        this.head = head.prev;
+
+        head.prev = undefined;
+
+        return head.value;
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
